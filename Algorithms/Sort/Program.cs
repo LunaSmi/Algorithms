38fa67ecc805f6b6870428a95eb60d61c5432d2f
Elena Smirnova <lena198smirnova@gmail.com>
2023-05-21 21:52:46 +0300

    BubbleSort

---
+++ Algorithms/Sort/Program.cs
@@ -0,0 +1,15 @@
+﻿using Sort;
+
+
+int[] array = { 1, 6, 5, 2, 3, 8, 4 };
+
+Sorting.BubbleSort(array);
+
+
+
+
+
+foreach (int i in array)
+{
+    Console.Write($"{i} ");
+}
---
+++ Algorithms/Sort/Sorting.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    public static class Sorting
+    {
+
+        public static void BubbleSort(int[] array)
+        {
+            for(int wall = array.Length - 1; wall > 0; wall--)
+            {
+                for(int i = 0; i < wall; i++)
+                {
+                    int j = i + 1;
+                    if (array[i] > array[j])
+                    {
+                        Swap(array,i,j);
+                    }
+                }
+            }
+        }
+
+        public static void SelectionSort(int[] array)
+        {
+
+        }
+
+        public static void InsertSort(int[] array)
+        {
+
+        }
+
+        public static void ShellSort(int[] array)
+        {
+
+        }
+
+        public static void MergeSort(int[] array)
+        {
+
+        }
+
+        public static void QuickSort(int[] array)
+        {
+
+        }
+
+
+
+
+        private static void Swap(int[] array,int i,int j)
+        {
+            if(i==j) return;
+
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+
+
+    }
+}
